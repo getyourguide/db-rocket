b9139831670ecad89da48fb1fff8ded5bf3f6739
JeanCarloMachado <jean.machado@getyourguide.com>
2021-03-03 22:44:50 +0100

    refactorings

--- databricks_local/databricks_local.py
+++ databricks_local/databricks_local.py
@@ -24,6 +24,30 @@ class DatabricksLocal:
 
         return self._build_and_deploy()
 
+    def _build(self):
+        """ builds a library with that project"""
+
+        dist_location = f"{self.project_location}/dist"
+        # cleans up dist
+        self._execute(f"rm {dist_location}/* || true")
+
+        self._execute(f"cd {self.project_location} ; python setup.py bdist_wheel")
+        self.wheel_file = self._execute(f"ls {dist_location} | head -n 1").replace("\n", "")
+        self.wheel_path = f"{dist_location}/{self.wheel_file}"
+        logging.info(f"Build Successful. Wheel: '{self.wheel_path}' ")
+
+    def _deploy(self):
+        """Deploys the version specified in config.yml"""
+        self._execute(
+            f"databricks fs cp --overwrite {self.wheel_path} {self.dbfs_folder}/{self.wheel_file}"
+        )
+
+        return f"""
+Great! in your notebook install the library by running: 
+
+%pip install {self.dbfs_folder}/{self.wheel_file}
+        """
+
     def _watch(self):
         cmd = f"watchmedo shell-command -w -W --interval 3 --patterns='*.py' --ignore-pattern='*build*'" \
               f" --recursive " \
@@ -44,44 +68,7 @@ class DatabricksLocal:
         return result
 
 
-    def _build(self):
-        """ builds a library with that project"""
-
-        dist_location = f"{self.project_location}/dist"
-        #cleans up dist
-        self._execute(f"rm {dist_location}/* || true")
-
-        self._execute(f"cd {self.project_location} ; python setup.py bdist_wheel")
-        self.wheel_file = self._execute(f"ls {dist_location} | head -n 1").replace("\n", "")
-        self.wheel_path = f"{dist_location}/{self.wheel_file}"
-        logging.info(f"Build Successful. Wheel: '{self.wheel_path}' ")
-
-
-    def _deploy(self):
-        """Deploys the version specified in config.yml"""
-        self._execute(
-            f"databricks fs cp --overwrite {self.wheel_path} {self.dbfs_folder}/{self.wheel_file}"
-        )
-
-        return f"""
-Great! in your notebook install the library by running: 
-
-%pip install {self.dbfs_folder}/{self.wheel_file}
-        """
-
-    def watch_and_deploy(self):
-        os.system('''
-            watchmedo shell-command --patterns="*.py" --recursive --command='./cli.py mlservice build_and_deploy' conduction_time
-        ''')
-
-    def list_deployed(self):
-        self._execute(f"databricks fs ls {self.directory_to_deploy_in}")
-
-
-    def perform(self):
-        self.build_and_deploy()
-
-    def install_db_connect(self):
+    def _install_db_connect(self):
         import os
         os.system("pip uninstall pyspark; pip install 'databricks-connect==7.3.9' ")
         #os.system("dbconnect configure")
