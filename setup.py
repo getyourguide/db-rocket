d5afc3c471372efce8a5017c6af7001a0d18bde3
JeanCarloMachado <jean.machado@getyourguide.com>
2021-04-12 22:33:13 +0200

    fix project email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="databricks-rocket",
     version="0.0.1",
     author="GetYourGuide",
-    author_email="engineering.data-productsgetyourguide.com",
+    author_email="engineering.data-products@getyourguide.com",
     description="Move code from your local machine to use in databricks during development",
     url="https://github.com/getyourguide/databricks-rocket",
     packages=setuptools.find_packages(),
