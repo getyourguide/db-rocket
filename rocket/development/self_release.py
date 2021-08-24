dc97ba45e1da3a06477de69582ed1373750b978e
JeanCarloMachado <jean.machado@getyourguide.com>
2021-08-24 18:05:11 +0200

    many improvements to the code structure and documentation

--- rocket/development/self_release.py
+++ rocket/development/self_release.py
@@ -1,6 +1,10 @@
 import os
 
 class SelfRelease:
+    """
+    Class responsible to serve the library in pypi, not part of rocket executable
+    """
+
     """Module responsible for building db-rocket itself and publishing it to pypi"""
     def build(self):
         """
--- rocket/rocket.py
+++ rocket/rocket.py
@@ -4,15 +4,30 @@ import subprocess
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout)
+
+def configure_logger() -> logging.Logger:
+    logger = logging.getLogger("dbrocket")
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    # use this for debug purposes
+    # logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
+    return logger
+
+logger = configure_logger()
 
 class Rocket:
-    _python_executable = 'python3'
-    _interval_repeat_watch = 3
+    """ Entry point of the installed program, all public methods are options of the program"""
+
     # in seconds
+    _interval_repeat_watch : int = 3
+    _python_executable : str = 'python3'
+    _rocket_executable : str = 'rocket'
+
+
     """rocket main executable"""
     def trigger(self, project_location: str, dbfs_path: str, watch=False):
         """
+        Entrypoint of the application, triggers a build and deploy
         :param project_location:
         :param dbfs_folder: path where the wheel will be stored, ex: dbfs:/tmp/myteam/myproject
         :return:
@@ -25,6 +40,7 @@ class Rocket:
         self.dbfs_folder = dbfs_path + project_directory
 
         if watch:
+            # first time build and then watch so we have an immediate build
             self._build_and_deploy()
             from time import sleep; sleep(self._interval_repeat_watch)
             return self._watch()
@@ -32,28 +48,32 @@ class Rocket:
         return self._build_and_deploy()
 
     def _build(self):
-        """ builds a library with that project"""
+        """
+        builds a library with that project
+        """
+        logger.info("Building python library")
 
         if not os.path.exists(f"{self.project_location}/setup.py"):
             raise Exception("To be turned into a library your project has to contain a setup.py file")
 
         dist_location = f"{self.project_location}/dist"
-        # cleans up dist
-        self._shell(f"rm {dist_location}/* || true")
+        # cleans up dist folder from previous build
+        self._shell(f"rm {dist_location}/* 2>/dev/null || true")
 
-        self._shell(f"cd {self.project_location} ; {self._python_executable} -m build")
+        self._shell(f"cd {self.project_location} ; {self._python_executable} -m build ")
         self.wheel_file = self._shell(f"cd {dist_location}; ls *.whl | head -n 1").replace("\n", "")
         self.wheel_path = f"{dist_location}/{self.wheel_file}"
-        logging.info(f"Build Successful. Wheel: '{self.wheel_path}' ")
+        logger.debug(f"Build Successful. Wheel: '{self.wheel_path}' ")
 
     def _deploy(self):
-        """Deploys the version specified in config.yml"""
+        """
+        Copies the built library to dbfs
+        """
         self._shell(
             f"databricks fs cp --overwrite {self.wheel_path} {self.dbfs_folder}/{self.wheel_file}"
         )
 
-        print(f"""
-Great! in your notebook install the library by running:
+        print(f"""Great! in your notebook install the library by running:
 
 %pip install {self.dbfs_folder.replace("dbfs:/","/dbfs/")}/{self.wheel_file} --force-reinstall
 
@@ -63,28 +83,45 @@ dbutils.library.install('{self.dbfs_folder}/{self.wheel_file}'); dbutils.library
 
         """)
 
-    def _watch(self):
-        cmd = f"watchmedo shell-command -w -W --interval {self._interval_repeat_watch} --patterns='*.py' --ignore-pattern='*build*;*egg-info*;*dist*'" \
-              f" --recursive " \
-              f"--command='rocket " \
-              f"trigger " \
-              f"{self.project_location} {self.dbfs_folder}' {self.project_location}"
-        logging.info(f'watch command: {cmd}')
-        print(os.system(cmd))
+    def _watch(self) -> None:
+        """
+        Listen to filesystem changes to build and deploy again
+        """
+        def get_command_without_watch() -> str:
+            """
+            Send to the watch command the command without the watch flag otherwise an infinite loop will be triggered
+            """
+            command = sys.argv
+            watch_flag = '--watch=True'
+            if watch_flag in command:
+                command.remove(watch_flag)
+            command = ' '.join(command)
+            logging.debug(command)
+            return command
 
-    def _send_notification(self, message):
+
+        cmd = f"""watchmedo \
+                shell-command \
+                --wait --drop  \
+                --patterns='*.py'  \
+                --recursive  \
+                --ignore-patterns '*/.*;build;*.egg-info;*dist' \
+                --command='{get_command_without_watch()}' 
+              """
+        logger.debug(f'watch command: {cmd}')
+        logger.debug(os.system(cmd))
+
+    def _send_notification(self, message) -> None:
         os.system(f"notify-send '{message}'")
 
     def _build_and_deploy(self):
-        logging.info("Starting to build")
         self._build()
         result = self._deploy()
         return result
 
-
     @staticmethod
     def _shell(cmd) -> str:
-        logging.info(f"Running shell command: {cmd} ")
+        logger.debug(f"Running shell command: {cmd} ")
         return subprocess.check_output(cmd, shell=True).decode("utf-8")
 
 
