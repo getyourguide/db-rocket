f7d90005717937c30108e69befcc624f1f8cf571
Steven Mi <steven.mi@getyourguide.com>
2022-03-21 17:03:58 +0100

    tests: add tests for rocket build

--- tests/test_rocket.py
+++ tests/test_rocket.py
@@ -1,9 +1,31 @@
 import unittest
 
+from rocket.rocket import Rocket
 
-class RocketTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+
+def test_build_python(python_rocket):
+    python_rocket._build()
+    assert python_rocket.wheel_file
+    assert python_rocket.wheel_path
+
+
+def test_build_poetry(poetry_rocket):
+    poetry_rocket._build()
+    assert poetry_rocket.wheel_file
+    assert poetry_rocket.wheel_path
+
+
+def test_build_wrong_project_location():
+    rocket = Rocket()
+    rocket.project_location = "/tmp"
+
+    try:
+        rocket._build()
+    except:
+        assert True
+
+    assert not hasattr(rocket, 'wheel_file')
+    assert not hasattr(rocket, 'wheel_path')
 
 
 if __name__ == '__main__':
