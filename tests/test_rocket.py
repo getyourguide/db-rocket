c95ae38e4fa465269310e4d2a6bc55f464e5ff1c
Steven <steven.mi@getyourguide.com>
2022-03-24 13:21:54 +0100

    tests: add short description to tests

--- tests/test_rocket.py
+++ tests/test_rocket.py
@@ -4,18 +4,27 @@ from rocket.rocket import Rocket
 
 
 def test_build_python(python_rocket):
+    """
+    Test if DB Rocket can build a python project
+    """
     python_rocket._build()
     assert python_rocket.wheel_file
     assert python_rocket.wheel_path
 
 
 def test_build_poetry(poetry_rocket):
+    """
+    Test if DB Rocket can build a poetry project
+    """
     poetry_rocket._build()
     assert poetry_rocket.wheel_file
     assert poetry_rocket.wheel_path
 
 
-def test_build_wrong_project_location():
+def test_build_raises_error():
+    """
+    Test if DB Rocket will raise an error if project is not a supported project
+    """
     rocket = Rocket()
     rocket.project_location = "/tmp"
 
