88b5ebf04e601f5cb7a67deb2a1d4f386e8bcffa
Steven Mi <steven.mi@getyourguide.com>
2023-08-11 12:55:51 +0800

    opt: simplify CLI & optimise flow

--- rocket/rocket.py
+++ rocket/rocket.py
@@ -6,8 +6,8 @@ import fire
 from watchdog.observers import Observer
 
 from rocket.logger import logger
-from rocket.utils import execute_shell_command, extract_project_name_from_wheel, \
-    extract_python_package_dirs, extract_python_files_from_folder, execute_for_each_multithreaded
+from rocket.utils import execute_shell_command, extract_python_package_dirs, extract_python_files_from_folder, \
+    execute_for_each_multithreaded
 from rocket.watcher import Watcher
 
 
@@ -79,85 +79,96 @@ setuptools.setup(
 
         self.dbfs_folder = dbfs_path + project_directory
 
-        if _deploy:
+        if not watch:
             self._build_and_deploy(watch)
+            return
+
+        if _deploy:
+            self._deploy(watch)
 
         if watch:
             observer = Observer()
             watcher = Watcher(observer)
             observer.schedule(watcher, project_location, recursive=True)
             observer.start()
-            try:
+            while True:
                 time.sleep(2)
-            finally:
-                observer.stop()
-            observer.join()
-            if watcher.modified_files:
-                self._deploy(watch=watch, modified_files=watcher.modified_files)
-            return execute_shell_command(
-                f'rocket launch --project_location={project_location} --watch={True} --dbfs_path={dbfs_path} _deploy=False')
+                if watcher.modified_files:
+                    observer.join()
+                    self._deploy(watch=watch, modified_files=watcher.modified_files)
+                    watcher.reset_modified()
 
     def _build_and_deploy(self, watch, modified_files=None):
         self._build()
-        result = self._deploy(watch=watch, modified_files=modified_files)
-        return result
+        self._deploy(watch=watch, modified_files=modified_files)
 
-    def _deploy(self, watch, modified_files):
+    def _deploy(self, watch, modified_files=None):
         """
         Copies the built library to dbfs
         """
-
         try:
-            if modified_files:
-                logger.info(f"Found changes in {modified_files}. Overwriting them.")
-                for file in modified_files:
-                    logger.info(f"Finished sync of {file}")
-                    execute_shell_command(
-                        f"databricks fs cp --recursive --overwrite {file} {self.dbfs_folder}/{os.path.relpath(file, self.project_location)}"
-                    )
-            else:
-                execute_shell_command(
-                    f"databricks fs cp --overwrite {self.wheel_path} {self.dbfs_folder}/{self.wheel_file}"
-                )
-                if watch:
-                    logger.info("You have watch activated. Your project will be automatically synchronised with databricks. Starting sync process:")
-                    package_dirs = extract_python_package_dirs(self.project_location)
-                    for package_dir in package_dirs:
-                        python_files = extract_python_files_from_folder(package_dir)
-
-                        def helper(file):
-                            execute_shell_command(
-                                f"databricks fs cp --recursive --overwrite {file} {self.dbfs_folder}/{os.path.relpath(file, self.project_location)}"
-                            )
-                            logger.info(f"Finished sync of {file}")
-                        execute_for_each_multithreaded(python_files, helper)
+            execute_shell_command(
+                f"databricks fs ls dbfs:/"
+            )
         except Exception as e:
             raise Exception(
-                f"Error while copying files to databricks, is your databricks token set and valid? Try to generate a new token and update existing one with `databricks configure --token`. Error details: {e}"
+                f"Error accessing DBFS via databricks-cli. Please check if your databricks token is set and valid? Try to generate a new token and update existing one with `databricks configure --token`. Error details: {e}"
             )
 
         base_path = self.dbfs_folder.replace("dbfs:/", "/dbfs/")
-        install_cmd = f'{base_path}/{self.wheel_file}'
-        install_cmd = _add_index_urls_to_cmd(install_cmd, self.index_urls)
-        project_name = extract_project_name_from_wheel(self.wheel_file)
+        if not watch:
+            install_cmd = f'{base_path}/{self.wheel_file}'
+            install_cmd = _add_index_urls_to_cmd(install_cmd, self.index_urls)
 
-        if watch:
-            logger.info(
-                f"""Sync completed. To use your library in your databricks notebook & automatically apply local changes add following in one cell:
+            execute_shell_command(
+                f"databricks fs cp --overwrite {self.wheel_path} {self.dbfs_folder}/{self.wheel_file}"
+            )
+            logger.info(f"""Uploaded your library to databricks. Install your library in your databricks notebook by running:
+            %pip install --upgrade pip
+            %pip install {install_cmd} --force-reinstall""")
+            return
+
+        if modified_files:
+            logger.info(f"Found changes in {modified_files}. Overwriting them.")
+            for file in modified_files:
+                logger.info(f"Finished sync of {file}")
+                execute_shell_command(
+                    f"databricks fs cp --recursive --overwrite {file} {self.dbfs_folder}/{os.path.relpath(file, self.project_location)}"
+                )
+            return
+
+        logger.info(
+            "You have watch activated. Your project will be automatically synchronised with databricks. Starting sync process:")
+        package_dirs = extract_python_package_dirs(self.project_location)
+        for package_dir in package_dirs:
+            python_files = extract_python_files_from_folder(package_dir)
+
+            def helper(file):
+                execute_shell_command(
+                    f"databricks fs cp --recursive --overwrite {file} {self.dbfs_folder}/{os.path.relpath(file, self.project_location)}"
+                )
+                logger.info(f"Finished sync of {file}")
+
+            execute_for_each_multithreaded(python_files, helper)
+
+        project_file = "setup.py"
+        if os.path.exists(f"{self.project_location}/pyproject.toml"):
+            project_file = "pyproject.toml"
+
+        execute_shell_command(
+            f"databricks fs cp --overwrite {self.project_location}/{project_file} {self.dbfs_folder}/"
+        )
+        logger.info(f"Finished sync of {self.project_location}/{project_file}")
+
+        logger.info(
+            f"""Sync completed. To use your library in your databricks notebook & automatically apply local changes add following in one cell:
 %pip install --upgrade pip
-%pip install {install_cmd} --force-reinstall
-%pip uninstall -y {project_name}
+%pip install -e {base_path}
 
 and then in a new Python cell:
 %load_ext autoreload
 %autoreload 2
-import sys
-import os
-sys.path.append(os.path.abspath('{base_path}')""")
-        else:
-            logger.info(f"""Uploaded your library to databricks. Install your library in your databricks notebook by running:
-    %pip install --upgrade pip
-    %pip install {install_cmd} --force-reinstall""")
+""")
 
     def _build(self):
         """
@@ -178,7 +189,6 @@ sys.path.append(os.path.abspath('{base_path}')""")
             if os.path.exists(f"{self.project_location}/requirements.txt"):
                 with open(f"{self.project_location}/requirements.txt") as f:
                     self.index_urls = [line.strip() for line in f.readlines() if "index-url" in line]
-
         elif os.path.exists(f"{self.project_location}/pyproject.toml"):
             logger.info("Found pyproject.toml. Building python library with poetry")
             execute_shell_command(f"cd {self.project_location} ; poetry build --format wheel")
--- rocket/watcher.py
+++ rocket/watcher.py
@@ -15,3 +15,6 @@ class Watcher(FileSystemEventHandler):
         if os.path.splitext(event.src_path)[1] == '.py':
             self.modified_files.append(event.src_path)
             self.observer.stop()
+
+    def reset_modified(self):
+        self.modified_files = []
